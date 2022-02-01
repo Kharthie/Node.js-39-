50b2a3558a63c98d1bd68fa0840825ec9da0d44b
Kharthie R <92292588+Kharthie@users.noreply.github.com>
2022-02-01 19:23:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-
+//1
 fs.readdir(process.argv[2], function (err, files) {
   files.forEach((file) => {
     if (err) throw err;
@@ -13,8 +13,10 @@ fs.readdir(process.argv[2], function (err, files) {
   });
 });
 
+// ↑  (OR)  ↓
 
 
+//2
 fs.readdir(process.argv[2], { withFileTypes: true }, function (err, allFiles) {
   if (err) throw err;
   const file = [];
@@ -26,10 +28,12 @@ fs.readdir(process.argv[2], { withFileTypes: true }, function (err, allFiles) {
       file.push(dirent.name);
     }
   });
-  console.log("Directory are --- ");
-  console.log(directory);
+ 
   console.log("File are --- ");
   console.log(file);
+  console.log("Directory are --- ");
+  console.log(directory);
 });
+
 
 
